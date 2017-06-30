306043bd11126b27adac5242986e95d6631a05ee
Baoyi Chen <cbywyy@gmail.com>
2017-06-30 13:11:34 -0700

    first version

---
+++ _site/hold.js
@@ -0,0 +1,14 @@
+(function($) {
+    $.fn.writeText = function(content) {
+        var contentArray = content.split(""),
+            current = 0,
+            elem = this;
+        setInterval(function() {
+            if(current < contentArray.length) {
+                elem.text(elem.text() + contentArray[current++]);
+            }
+        }, 100);
+    };
+    
+})(jQuery);
+$("#holder").writeText("This is some text");
